119c1bb6906534b12edf8bb0bac9a7cca7b16d5a
world324digital <70345255+world324digital@users.noreply.github.com>
2022-01-24 15:18:07 +0300

    add search event

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 console.log('our app code goes here');
+$("#search").click(function(e){
+	e.preventDefault();
+	$('#display').empty();
+fetch("https://restcountries-v1.p.rapidapi.com/name/" + searchValue ,{
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+		"x-rapidapi-key": "9f24baf32fmshbb4ae565ad2c065p11499ajsn78a07e72d500"
+	}
+})
+.then(function(response) {
+ })
+ .then(function(data) {
+ })
+});
